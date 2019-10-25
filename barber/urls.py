e1f136a69f063b8a7c59fd7967415bc6f5bbee0c
Eduardo <Eduardo@EDUARDO-PC>
2019-10-25 20:55:05 -0300

    agrego proyecto

---
+++ barber/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name="inicio"),
+    path('login/', views.login, name="login"),
+    path('registrarCliente/', views.ClienteCreate.as_view(),
+         name="registrarCliente"),
+    path('login/cliente/', views.cliente, name="cliente"),
+    path('login/admin/', views.admin, name="administrador"),
+    path('actualizarCliente/<int:cliente_id>',
+         views.ClienteActualizar, name="actualizarCliente"),
+    path('borrarCliente/<int:cliente_id>',
+         views.ClienteBorrar, name="borrarCliente"),
+    path('listarCliente/', views.ClienteList.as_view(), name="listarCliente"),
+    path('productoCliente/', views.FotosView, name="productoCliente"),
+    path('categoriaProducto/', views.CategoriaView.as_view(),
+         name="categoriaProducto"),
+    path('listarProductos/',views.ProductoList.as_view(),name="listarProductos"),
+]
